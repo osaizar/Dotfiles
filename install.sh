15f78120cb8faca81043ff97aab07aee67dfba47
osaizar <oisaizar@gmail.com>
2025-01-12 23:13:16 +0100

    robby theme + hostname for servers

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ sudo apt install zsh tmux git curl
 # 2. ohmyzsh
 echo "[+] Installing ohmyzsh you may need to exit zsh after the installation is complete"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp zshrc ~/.zshrc
+cp zsh/zshrc ~/.zshrc
+cp zsh/*.zsh-theme ~/.oh-my-zsh/themes
 
 # 3. tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp tmux.conf ~/.tmux.conf
+
 echo "[+] To install the tmux plugins open a tmux session and press ctrl-b + I"
 
 echo "[+] Done, now set your terminal emulators first command to: 'tmux new-session -A -s main'" 
